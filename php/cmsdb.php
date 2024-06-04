8a5aeea4aabec032a0221e6571976f434c2fbd42
張翊飛 <francis810712@hotmail.com>
2024-06-04 20:24:33 +0800

    dbname 變更 for 作業06

--- php/cmsdb.php
+++ php/cmsdb.php
@@ -2,7 +2,7 @@
 $dbhost = "127.0.0.1";  // MySQL主機位址，localhost=本機
 $dbpass1 = "a110e109";  // MySQL帳號
 $dbpass2 = "a110e109";  // MySQL密碼
-$dbname = "dba110e109_05"; // 資料庫名稱
+$dbname = "dba110e109_06"; // 資料庫名稱
 $conn  = new mysqli($dbhost, $dbpass1, $dbpass2, $dbname); // 連結MySQL資料庫
 /* 檢查連線 */
 if ($conn->connect_error) {
